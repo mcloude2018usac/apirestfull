dbfbfc03e73ac0e7dfdfd0af567e0e221ccaa676
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-02-05 21:19:10 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
- //  'url': 'mongodb://usac2:N0T!3n3@ds111618.mlab.com:11618/usac'
-    'url': 'mongodb://usac:Admin123@ds253831.mlab.com:53831/usac'  
+   'url': 'mongodb://usac2:N0T!3n3@ds111618.mlab.com:11618/usac'
+ //   'url': 'mongodb://usac:Admin123@ds253831.mlab.com:53831/usac'  
 }
