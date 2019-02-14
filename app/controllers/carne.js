93bc6497fb4b4fd67f183dba6f4f0fcaf9d7f0bb
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-02-13 22:40:38 -0600

    Add files via upload

--- app/controllers/carne.js
+++ app/controllers/carne.js
@@ -3,9 +3,11 @@ var Carne = require('../models/carne');
 
 
 exports.getCarne = function(req, res, next){
-    if(req.params.codigo)
-    { console.log(req.params.codigo)
-        Carne.find({carne:req.params.codigo},function(err, todos) {
+
+    
+    if(req.params.id)
+    { 
+        Carne.find({cui:req.params.codigo},function(err, todos) {
             if (err){ res.send(err); }
         
             if(todos.length>0)   {    res.json(todos);   }
@@ -18,7 +20,27 @@ exports.getCarne = function(req, res, next){
     else
     {
 
+        if(req.params.codigo)
+        { 
+            Carne.find({carne:req.params.codigo},function(err, todos) {
+                if (err){ res.send(err); }
+            
+                if(todos.length>0)   {    res.json(todos);   }
+                else
+                {  res.status(500).send('NO EXISTE REGISTRO');      }
+                
+            });
+      
+        }
+        else
+        {
+    
+        }
+    
+
     }
+    
+
     
     
  
