ddd6f344ceb63388e04ecb4361416f30864914e3
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-04-25 07:40:34 -0600

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
- 
-  url:'mongodb://usac:N0Ti3n3@ds133935-a0.mlab.com:33935/usac2?replicaSet=rs-ds133935' 
- 
+//   'url': 'mongodb://usac2:N0T!3n3@ds111618.mlab.com:11618/usac'
+ // 'url': 'mongodb://usac:Admin123@ds253831.mlab.com:53831/usac' 
+ url:'mongodb://usac:N0Ti3n3@ds133935-a0.mlab.com:33935/usac2?replicaSet=rs-ds133935' 
+    
 }
