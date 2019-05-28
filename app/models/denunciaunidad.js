d9a507fd3a857b23192230c11d0f1bea3d8a0c2a
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-05-28 14:15:17 -0600

    Add files via upload

--- app/models/denunciaunidad.js
+++ app/models/denunciaunidad.js
@@ -1,9 +1,12 @@
 var mongoose =require("mongoose");
     var DenunciaunidadSchema 		=	new  mongoose.Schema({
-        categoria	: { type : String, required : true , ref:'Dcatalogo' },
+        categoria	:  [{ _id:String, nombre:String}   ]
+                 ,
         unidad	: { type : String, required : true , ref:'Catalogo' },
         nombre	: { type : String, required : true },
         estado	: { type : String },
+        jefeop	: { type : String, required : true , ref:'User' },
+        operadores	:  [{ _id:String, nombre:String}   ],
         usuarionew	: { type : String },      usuarioup	: { type : String }
         }, {
         timestamps: true
