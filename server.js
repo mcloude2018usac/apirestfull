594baccb5ea7a21031b569ca2c042f951e025977
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-06-11 15:22:10 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ var router = require('./app/routes');
 
 var mailt = require('./app/controllers/mail');
 
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./config/usacenlinea-firebase-adminsdk-oj0bn-c6de33b49f.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://usacenlinea.firebaseio.com"
+});
+
 //mongoose.set('useCreateIndex', true);
 mongoose.connect(databaseConfig.url, { useNewUrlParser: true ,useCreateIndex: true });
 
@@ -73,6 +83,63 @@ function errorHandler(err, req, res, next) {
   res.status(500);
   res.render('error', { error: err });
 }
+
+/*
+
+var registrationToken = "eLrosvO4YMg:APA91bEmdUFHl0GrZizado454Jru-hWb6tLoLOzGdn8VJNNsYvUdkHoWrntq7Vj26VWZooGQmNj0Wy_NJBDgdSa7LanHPnQSjMnbT_cVF95V19mpjIKXnQcm8Ed6pgR2JJyNQu8Bppfn";
+
+
+var message = {
+  data: {
+    score: '850',
+    time: '2:45'
+  },
+  token: registrationToken
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
+*/
+  /*
+
+
+  var payload = {
+        notification: {
+          title: message.title,
+          body: message.body,
+          icon: "https://placeimg.com/250/250/people"
+        }
+      };
+
+
+var payload = {
+  notification: {
+    title: "This is a Notification",
+    body: "This is the body of the notification message."
+  }
+};
+
+var options = {
+  priority: "high",
+  timeToLive: 60 * 60 *24
+};
+
+admin.messaging().sendToDevice(registrationToken, payload, options)
+  .then(function(response) {
+    console.log("Successfully sent message:", response);
+  })
+  .catch(function(error) {
+    console.log("Error sending message:", error);
+  });
+*/
 
 //correop.mandacorreoprueba();
 
