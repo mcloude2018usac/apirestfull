6600cf94f06b02fa374910b3576f95f82476a60a
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-06-19 07:01:36 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 var express  = require('express');
 
 const cron = require("node-cron");
@@ -109,72 +109,28 @@ function errorHandler(err, req, res, next) {
   res.status(500);
   res.render('error', { error: err });
 }
-
-
-/*
-
-var registrationToken = "eLrosvO4YMg:APA91bEmdUFHl0GrZizado454Jru-hWb6tLoLOzGdn8VJNNsYvUdkHoWrntq7Vj26VWZooGQmNj0Wy_NJBDgdSa7LanHPnQSjMnbT_cVF95V19mpjIKXnQcm8Ed6pgR2JJyNQu8Bppfn";
-
-
-var message = {
-  data: {
-    score: '850',
-    time: '2:45'
-  },
-  token: registrationToken
-};
-
-// Send a message to the device corresponding to the provided
-// registration token.
-admin.messaging().send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
-  });
-*/
-  /*
-
-
-  var payload = {
-        notification: {
-          title: message.title,
-          body: message.body,
-          icon: "https://placeimg.com/250/250/people"
-        }
-      };
-
-
-var payload = {
-  notification: {
-    title: "This is a Notification",
-    body: "This is the body of the notification message."
-  }
-};
-
-var options = {
-  priority: "high",
-  timeToLive: 60 * 60 *24
-};
-
-admin.messaging().sendToDevice(registrationToken, payload, options)
-  .then(function(response) {
-    console.log("Successfully sent message:", response);
-  })
-  .catch(function(error) {
-    console.log("Error sending message:", error);
-  });
 */
 
-//correop.mandacorreoprueba();
+var querystring = require('querystring');
+var MailParser = require("mailparser").MailParser;
 
-/*
-
-// Instruct node to run the socket server on the following port
-http.listen(process.env.PORT || 9191, function()
-{
-  console.log('listening on port 9191' );
+var server = require('http').createServer();
+server.addListener('request', function(req, res) {
+  var chunks = [];
+  req.on('data', chunks.push.bind(chunks));
+  req.on('end', function() {
+    var mailparser = new MailParser();
+    mailparser.on("end", function(mail_object) {
+      // TODO: use 'mail_object'
+      // see API for https://github.com/andris9/mailparser
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.end();
+    });
+    var params = querystring.parse(chunks.join("").toString());
+    mailparser.write(params['message']);
+    mailparser.end();
+  });
 });
-*/
+var port = process.env.PORT || 3000;
+console.log(' [*] Listening on 0.0.0.0:' + port);
+server.listen(port, '0.0.0.0');
