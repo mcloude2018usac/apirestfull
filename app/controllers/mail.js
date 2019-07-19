7dd0b46f17d57836e551796a61ee3c417ca3f3e4
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-07-19 12:46:15 -0600

    Add files via upload

--- app/controllers/mail.js
+++ app/controllers/mail.js
@@ -33,14 +33,14 @@ exports.getMail = function(req, res, next){
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-               user: 'usacenlinea1.0@usacenlinea.net',
-              // pass: 'ocitocit'
-               pass: 'Amazon1518@'
+               user: 'usacenlinea@gmail.com',
+               pass: 'Nu3va3ra2021@'
+            //  pass: 'Amazon1518@'
            }
        });
         
       const mailOptions = {
-        from: 'usacenlinea1.0@usacenlinea.net', // sender address
+        from: 'usacenlinea@gmail.com', // sender address
         to: req.body.destino+';mario.morales@mcloude.com', // list of receivers
         subject: req.body.subjet, // Subject line
         html: req.body.html
