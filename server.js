4a401e5b9dc620d5621529721e9815e069392d95
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-07-18 23:16:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express  = require('express');
 
 const cron = require("node-cron");
@@ -16,30 +15,40 @@ var databaseConfig = require('./config/database');
 var router = require('./app/routes');
 
 var mailt = require('./app/controllers/mail');
-const throng = require('throng')
 
-const WORKERS = process.env.WEB_CONCURRENCY || 1
 
-throng({
-  workers: WORKERS,
-  lifetime: Infinity
-}, start)
+var admin = require("firebase-admin");
 
-function start() {
+var serviceAccount = require("./config/usacenlinea-firebase-adminsdk-oj0bn-c6de33b49f.json");
 
-  mongoose.connect(databaseConfig.url, { useNewUrlParser: true ,useCreateIndex: true });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://usacenlinea.firebaseio.com"
+});
+
+//mongoose.set('useCreateIndex', true);
+mongoose.connect(databaseConfig.url, { useNewUrlParser: true ,useCreateIndex: true });
+
+  // schedule tasks to be run on the server
   cron.schedule("59 23 * * *", function() {
+   
     crntt.mandaeventos();
+  
+   /* fs.unlink("./error.log", err => {
+      if (err) throw err;
+   
+    });
+    */
   });
-app
-.get('/cpu', cpuBound)
-.get('/memory', memoryBound)
-.get('/io', ioBound)
-.listen(process.env.PORT || 9090);
-console.log("App listening on port 9090");
 
-//app.use(express.static('app/controllers'));
-app.use(express.static('public'));
+ 
+app.listen(process.env.PORT || 9090);
+console.log("App listening on port 9090");
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+
+//app.use(express.methodOverride());
+
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json({limit: '50mb'})); // Send JSON responses
 app.use(methodOverride());
@@ -50,45 +59,9 @@ app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Rest api full 5.0!');
+  res.status(500).send('Something broke!');
 }); 
 router(app);
-
-
-
-
-  function hello(req, res, next) {
-    res.send('Hello, world')
-  }
-
-  function cpuBound(req, res, next) {
-    const key = Math.random() < 0.5 ? 'ninjaturtles' : 'powerrangers'
-    const hmac = crypto.createHmac('sha512WithRSAEncryption', key)
-    const date = Date.now() + ''
-    hmac.setEncoding('base64')
-    hmac.end(date, () => res.send('A hashed date for you! ' + hmac.read()))
-  }
-
-  function memoryBound(req, res, next) {
-    const large = Buffer.alloc(10 * 1024 * 1024, 'X')
-    setTimeout(() => {
-      const len = large.length  // close over the Buffer for 1s to try to foil V8's optimizations and bloat memory
-      console.log(len)
-    }, 1000).unref()
-    res.send('Allocated 10 MB buffer')
-  }
-
-  function ioBound(req, res, next) {
-    setTimeout(function SimulateDb() {
-      res.send('Got response from fake db!')
-    }, 300).unref()
-  }
-
-  function onListen() {
-    console.log('Listening on', PORT)
-  }
-}
-
 
 
 
@@ -110,31 +83,3 @@ function errorHandler(err, req, res, next) {
   res.status(500);
   res.render('error', { error: err });
 }
-
-//correop.massMailer(); 
-/*
-
-var querystring = require('querystring');
-var MailParser = require("mailparser").MailParser;
-
-var server = require('http').createServer();
-server.addListener('request', function(req, res) {
-  var chunks = [];
-  req.on('data', chunks.push.bind(chunks));
-  req.on('end', function() {
-    var mailparser = new MailParser();
-    mailparser.on("end", function(mail_object) {
-      // TODO: use 'mail_object'
-      // see API for https://github.com/andris9/mailparser
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.end();
-    });
-    var params = querystring.parse(chunks.join("").toString());
-    mailparser.write(params['message']);
-    mailparser.end();
-  });
-});
-var port = 3000;
-console.log(' [*] Listening on 0.0.0.0:' + port);
-server.listen(port, '0.0.0.0');
-*/
