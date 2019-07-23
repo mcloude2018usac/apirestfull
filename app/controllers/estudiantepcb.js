195265dd225eba86a83e2cff4814d98bf1ba82b7
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-07-23 09:56:39 -0600

    Add files via upload

--- app/controllers/estudiantepcb.js
+++ app/controllers/estudiantepcb.js
@@ -7,9 +7,44 @@ exports.getEstudiantepcb = function(req, res, next){
     { 
 
         Estudiantepcb.find({ORIENTACION:req.params.codigo}).then(todos => {
-         
+                   
+       
                 res.json(todos);  
             
+            
+          
+      })
+      .catch(err => {
+          res.status(500).send(err.message);  
+      })
+   
+
+
+  
+    }
+    else
+    {
+
+    }
+    
+    
+ 
+}
+//para las impresiones
+exports.getEstudiantepcb2 = function(req, res, next){
+    if(req.params.codigo)
+    { 
+        console.log('el resultado')
+
+        Estudiantepcb.find({ORIENTACION:req.params.codigo}).then(todos => {
+                   
+            if(todos.length==0)
+            {
+                res.status(500).send('Resultados no existen');  
+            }
+            else{
+                res.json(todos);  
+            }
             
           
       })
