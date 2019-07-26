3e6962b616dd76d9cad630b6cdbeb0ddc6ffeae5
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-07-25 22:22:05 -0600

    Add files via upload

--- app/controllers/asignapcb.js
+++ app/controllers/asignapcb.js
@@ -170,7 +170,7 @@ exports.getAsignapcb = function(req, res, next){
           
 
                 
-                Facplan.find({_id: {$in: duplicates},'idperiodo.nombre':'2019-02'}).sort({   "asignados" : -1    }).select({ "idtipounidad.nombre":1,"idunidadacademica.nombre":1,"idedificio.nombre":1,"idsalon.nombre":1,"capacidad":1,"asignados":1}).exec(function(err, todos) {
+                Facplan.find({_id: {$in: duplicates},'idperiodo.nombre':'2019-01'}).sort({   "asignados" : -1    }).select({ "idtipounidad.nombre":1,"idunidadacademica.nombre":1,"idedificio.nombre":1,"idsalon.nombre":1,"capacidad":1,"asignados":1}).exec(function(err, todos) {
                     if (err){ res.send(err); }
                     var cad='';
                     for(var i = 0; i < todos.length;i++){
@@ -458,7 +458,7 @@ if(req.params.recordID!=='crea')
 else{
 
 var aa=1;
-if(aa==0) 
+if(aa==1) 
 {
     res.status(404).send('Las fechas de inscripción PCB han finalizado.');
 
