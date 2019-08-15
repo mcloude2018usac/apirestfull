9efe6134adb35c4a6f1b0b681aa488a7ecb90070
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-08-15 14:05:54 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,3 @@
 module.exports = {
-//   'url': 'mongodb://usac2:N0T!3n3@ds111618.mlab.com:11618/usac'
- // 3 colect 'url': 'mongodb+srv://usac2018:N0T!3n3@cluster0-22hs7.mongodb.net/test?retryWrites=true&w=majority' 
-//url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority'
-url: 'mongodb+srv://usac2018:N0T!3n3@cluster0-22hs7.mongodb.net/test?retryWrites=true&w=majority' 
+url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority'
 }
