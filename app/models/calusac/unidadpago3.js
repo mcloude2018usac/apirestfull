101c148a1cfb0408f9574245910a304a63e47430
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-09-19 09:53:43 -0600

    Add files via upload

--- app/models/calusac/unidadpago3.js
+++ app/models/calusac/unidadpago3.js
@@ -3,7 +3,7 @@ var mongoose =require("mongoose");
     var Unidadpago3Schema 		=	new  mongoose.Schema({
         idtipounidad   		: { type : String, required : true },
         idunidadacademica   		: { type : String, required : true },
-        codigo	: { type : String, required : true },
+        codigo	: { type : Number, required : true },
         tipo	: { type : String, required : true },
         nombre	: { type : String, required : true },
         nivel	: { type : String, required : true,ref:'Unidadnivel3' },
