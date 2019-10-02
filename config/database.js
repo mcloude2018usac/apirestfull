d6b62a90f16a4bf7773b0d7bcbf667f01d7d0911
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-10-02 13:06:58 -0600

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
-  url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority' 
- //   url:'mongodb+srv://usac2018:N0T!3n3@cluster0-22hs7.mongodb.net/test?retryWrites=true&w=majority' 
-  
-
+ url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority' // toda lada
+    
+    
 }
