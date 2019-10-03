cf2e1aaabfd440b7653aef2c380bd155630bb351
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-10-03 10:03:51 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,112 +1,142 @@
-var port    = process.env.PORT || 3000,
-    app 	= require('express')(),
-	http 	= require('http').Server(app),
-   io 		= require('socket.io')(http);
-   var databaseConfig = require('./config/database');
-   var mongoose = require('mongoose');
-   mongoose.connect(databaseConfig.url);
-   
 
-   var Usermsg = require('./app/models/usermsg');
+var express  = require('express');
+
+const cron = require("node-cron");
+var crntt = require('./app/controllers/crontabfunc');
+var correop= require('./app/controllers/mailprueba');
+//const fs = require("fs");
+var app      = express();
+var mongoose = require('mongoose');
+var logger = require('morgan');
+
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cors = require('cors');
+var databaseConfig = require('./config/database');
+var router = require('./app/routes');
+
+var mailt = require('./app/controllers/mail');
+const throng = require('throng')
+
+const WORKERS = process.env.WEB_CONCURRENCY || 1
+process.env.NODE_TLS_REJECT_UNAUTHORIZED=1
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start)
+
+function start() {
+
+  mongoose.connect(databaseConfig.url, { useNewUrlParser: true ,useCreateIndex: true });
+  cron.schedule("59 23 * * *", function() {
+    crntt.mandaeventos();
+  });
+app
+.get('/cpu', cpuBound)
+.get('/memory', memoryBound)
+.get('/io', ioBound)
+.listen(process.env.PORT || 9090);
+console.log("App listening on port 9090");
+
+//app.use(express.static('app/controllers'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false })); // Parses urlencoded bodies
+app.use(bodyParser.json({limit: '50mb'})); // Send JSON responses
+app.use(methodOverride());
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+app.use(logger('dev')); // Log requests to API using morgan
+app.use(cors());
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Rest api full 5.0!');
+}); 
+router(app);
 
 
-// Allow CORS support and remote requests to the service
-app.use(function(req, res, next)
-{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
-    next();
+
+
+  function hello(req, res, next) {
+    res.send('Hello, world')
+  }
+
+  function cpuBound(req, res, next) {
+    const key = Math.random() < 0.5 ? 'ninjaturtles' : 'powerrangers'
+    const hmac = crypto.createHmac('sha512WithRSAEncryption', key)
+    const date = Date.now() + ''
+    hmac.setEncoding('base64')
+    hmac.end(date, () => res.send('A hashed date for you! ' + hmac.read()))
+  }
+
+  function memoryBound(req, res, next) {
+    const large = Buffer.alloc(10 * 1024 * 1024, 'X')
+    setTimeout(() => {
+      const len = large.length  // close over the Buffer for 1s to try to foil V8's optimizations and bloat memory
+      console.log(len)
+    }, 1000).unref()
+    res.send('Allocated 10 MB buffer')
+  }
+
+  function ioBound(req, res, next) {
+    setTimeout(function SimulateDb() {
+      res.send('Got response from fake db!')
+    }, 300).unref()
+  }
+
+  function onListen() {
+    console.log('Listening on', PORT)
+  }
+}
+
+
+
+
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something failed!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+//correop.mandacorreoprueba2()
+//correop.mandanoti(); 
+/*
+
+var querystring = require('querystring');
+var MailParser = require("mailparser").MailParser;
+
+var server = require('http').createServer();
+server.addListener('request', function(req, res) {
+  var chunks = [];
+  req.on('data', chunks.push.bind(chunks));
+  req.on('end', function() {
+    var mailparser = new MailParser();
+    mailparser.on("end", function(mail_object) {
+      // TODO: use 'mail_object'
+      // see API for https://github.com/andris9/mailparser
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.end();
+    });
+    var params = querystring.parse(chunks.join("").toString());
+    mailparser.write(params['message']);
+    mailparser.end();
+  });
 });
-
-
-// Set up route
-app.get('/', (req, res) =>
-{
-   res.json({ message: 'hello world' });
-});
-
-
-
-io.on('connection', (socket) =>
-{
-
-    
-  socket.on('disconnect', function(){
-   io.emit('users-changed', {user: socket.nickname, event: 'left'});   
- });
-
- socket.on('set-nickname', (nickname) => {
-   socket.nickname = nickname;
-   io.emit('users-changed', {user: nickname, event: 'joined'});    
- });
-    
- socket.on('add-message', (message) => {
-  console.log(message)
-   io.emit('message', {text: message.text, messageimg  	: message.imagen,text2: message.text2,imagen2:message.imagen2, from: socket.nickname, created: new Date()});   
-
-   Usermsg.create({  userId :   message.userId ,
-   toUserId:  	 message.toUserId,
-   status	: 'Pendiente',
-   message  	: message.text,
-   messageimg  	: message.imagen,
-   message2  	: message.text2,
-   userId2 :   message.userId2 ,
-   imagen2:message.imagen2
-      
-     });
-
-   
- });
-
- 
- socket.on('add-image', (message)=>
- {
-     io.emit('message', { image: message.image, sender: socket.nickname,  created: new Date() });
- });
-
- /*
- 
-   socket.on('disconnect', ()=>
-   {
-       io.emit('user-exited', { user: socket.alias });
-   });
-
-
-
-
-   socket.on('add-message', (message)=>
-   {
-       io.emit('message', { message: message.message, sender: socket.alias, tagline: socket.handle, location: socket.location, published: new Date() });
-   });
-
-
-
-   socket.on('add-image', (message)=>
-   {
-       io.emit('message', { image: message.image, sender: socket.alias, tagline: socket.handle, location: socket.location, published: new Date() });
-   });
-
-
-
-   socket.on('set-alias', (obj)=>
-   {
-      
-   	  socket.alias 		= obj.alias;
-   	  socket.handle 	= obj.handle;
-   	  socket.location 	= obj.location;
-      io.emit('alias-added', { user: obj.alias, tagline: obj.handle, location: obj.location });
-   });
-
+var port = 3000;
+console.log(' [*] Listening on 0.0.0.0:' + port);
+server.listen(port, '0.0.0.0');
 */
-
-
-
-});
-
-
-// Instruct node to run the socket server on the following port
-http.listen(port, function()
-{
-  console.log('listening on port ' + port);
-});
