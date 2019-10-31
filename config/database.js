3027c8ea7bf8df72999d637695b82e3b9729c744
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-10-31 07:42:58 -0600

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,5 +1,13 @@
 module.exports = {
+//   'url': 'mongodb://usac2:N0T!3n3@ds111618.mlab.com:11618/usac'
+
  url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority' // toda lada
-    
-    
+ // url: 'mongodb+srv://usac2018:N0T!3n3@cluster0-22hs7.mongodb.net/test?retryWrites=true&w=majority' //ovs y pcbs
+
+
+
+
+
+//-----------------------------------------para carnes
+//mongodb+srv://usac:6zdvLMGFvaN7ohHD@cluster0-labwq.mongodb.net/test
 }
