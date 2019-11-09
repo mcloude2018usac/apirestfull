3fce55b435a34a8c468fe3ab287d01c855b924d1
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-11-09 17:25:14 -0600

    Add files via upload

---
+++ app/models/calusac/unidadplan4.js
@@ -0,0 +1,41 @@
+var mongoose =require("mongoose");
+
+
+    var Unidadplan4Schema 		=	new  mongoose.Schema({
+        idtipounidad :  		{    id	: { type : String, required : true },   nombre	: { type : String, required : true }        },
+        idperiodo   		: {    id	: { type : String, required : true },   nombre	: { type : String, required : true }        },
+        idedificio: {    id	: { type : String, required : true },   nombre	: { type : String, required : true }        },
+        idsalon: {    id	: { type : String, required : true },   nombre	: { type : String, required : true }        },
+        nombre: {
+            type: String,
+            required: true
+        },
+        ididioma: {
+            type: String,
+            required: true,ref:'Unidadidioma3'
+        },
+        idhora: {  type: String,   required: true   },
+        iddia: {  type: Date,   required: true   },
+        idprofesor: {
+            type: String,
+            required: true,ref:'User'
+        },
+        capacidad: {
+            type: Number,
+            required: true
+        },
+        asignados: {
+            type: Number
+        },
+
+        date 		: { type: Date, default: Date.now }
+        ,usuarionew	: { type : String },      usuarioup	: { type : String }
+        }, {
+        timestamps: true
+        });
+
+      
+
+module.exports = mongoose.model('Unidadplan4', Unidadplan4Schema);
+
+
