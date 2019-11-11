d4585d8cc4f7ed55e4afe803ca66d1405581baf8
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-11-10 21:42:32 -0600

    Add files via upload

--- app/models/solcarne.js
+++ app/models/solcarne.js
@@ -3,6 +3,7 @@ var mongoose =require("mongoose");
         idempresa   		: { type : String, required : true },
         idsuscriptor :  		{    id	: { type : String, required : true, ref: 'User' },   nombre	: { type : String, required : true }        },
         nombre	: { type : String, required : true },
+        codigo	: { type : String, required : true },
         estado	: { type : String, required : true },
         nota   		: { type : String}
         ,usuarionew	: { type : String },      usuarioup	: { type : String }
