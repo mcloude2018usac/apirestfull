b50e932f6c66f5b3bc63cf82c6d31af007cefd65
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2019-11-22 15:19:59 -0600

    Update oraclesqlx.js

--- app/controllers/oraclesqlx.js
+++ app/controllers/oraclesqlx.js
@@ -3,9 +3,9 @@ var Indicadore = require('../models/segeplan/indicadores');
 
 
 var connAttrs = {
-    "user": "dbindicadores",
-    "password": "N0tiene",
-    "connectString": "190.143.151.236/xe"
+       "user": "dbindicadores",
+    "password": "S1t13n3$",
+    "connectString": "181.174.122.180/xe"
 }
 
 function removeDups(names) {
