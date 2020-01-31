f7a0e02db141c51d8e3a01a1a3470520c781a177
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-01-31 12:25:11 -0600

    Add files via upload

--- app/models/asignapap.js
+++ app/models/asignapap.js
@@ -8,57 +8,24 @@ var AsignapapSchema = new mongoose.Schema({
     monto: {        type: Number,        required: true    },
     montodeuda: {        type: Number,        required: true    },
     cursosaplica: {        type: String,        required: true    },
-    
-    
-      cierra  		: { type : String },
-    
     rubro   		: { type : String },
     llave   		: { type : String },
-
-    
-    
+    cierra   		: { type : String },
     nov   		: { type : String },
     carne   		: { type : String },
-
-
-
+    fechasiif	: { type : String },
     lenguaje2	: { type : Boolean },
-
-
     idpago: { type : String },
-
-
-
-    
-    
-    
-    
-    
-    
-    
     idjornada: {        type: String    },
-  
-    
-
-
-
-
-
-
-
-
-
     idjornada2: { type : String },
     idjornada3: { type : String },
     idjornada4: { type : String },
     idjornada5: { type : String },
-
     idhorario: { type : String },
     idhorario2: { type : String },
     idhorario3: { type : String },
     idhorario4: { type : String },
     idhorario5: { type : String },
-
     lenguaje	: { type : Boolean },
     matematica	: { type : Boolean },
     fisica	: { type : Boolean },
@@ -67,8 +34,6 @@ var AsignapapSchema = new mongoose.Schema({
     nota: {        type: String,        required: true    },
     correo: {        type: String,        required: true    },
     estado: {        type: String,        required: true    },
- 
-  
     usuarionew	: { type : String },      usuarioup	: { type : String }
 }, {
     timestamps: true
