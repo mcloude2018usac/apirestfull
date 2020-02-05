89b8b3313da8cb349846c54a7f4fe553e6e6070a
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-02-05 09:39:36 -0600

    Update asignapap.js

--- app/controllers/asignapap.js
+++ app/controllers/asignapap.js
@@ -335,7 +335,7 @@ else{
     if(req.params.recordID=='crea')
 { 
 
-        var aa=1;
+        var aa=0;
 if(aa==1) 
 {
     res.status(404).send('Las fechas de generacion de boleta PAP han finalizado.');
