18b25fc601e23317303de612775fcac5293235b7
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-04-30 19:44:22 -0600

    Add files via upload

--- app/controllers/calusac/asignaubicacion.js
+++ app/controllers/calusac/asignaubicacion.js
@@ -93,7 +93,7 @@ exports.getAsignaubicacion = function(req, res, next){
                 Asignacalusac.find({idplanifica:req.params.id2
                 
             }).populate('ididioma')  .select({ "nombre":1,"cui": 1,"identificador": 1,"n1": 1,"n2": 1,"n3": 1,
-            "n4":1,"n5":1,       "_id": 1}).exec(function(err, todos10) {
+            "n4":1,"n5":1,  "profedor":"1",     "_id": 1}).exec(function(err, todos10) {
                     if (err){ res.send(err); }
        
                     res.json(todos10);
