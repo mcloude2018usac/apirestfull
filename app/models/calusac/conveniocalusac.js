3460f554f67af2a6f88fc5ba59026a2e1912b9eb
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-05-16 10:00:18 -0600

    Add files via upload

---
+++ app/models/calusac/conveniocalusac.js
@@ -0,0 +1,28 @@
+var mongoose =require("mongoose");
+    var ConveniocalusacSchema 		=	new  mongoose.Schema({
+            
+        idempresa	: { type : String, required : true },
+        f1	: { type : Date, required : true},
+        f2	: { type : Date, required : true},
+        tipo	: { type : String, required : true},
+        nombre	: { type : String, required : true },
+        monto	: { type : Number, required : true },
+
+        estado:{ type : String},
+        noboletapago:{ type : String },
+        fechasiif:{ type : String },
+        llave:{ type : String },
+        identificador:{ type : String},
+
+
+
+        usuarionew	: { type : String },      usuarioup	: { type : String }
+        }, {
+        timestamps: true
+        });
+
+      
+
+module.exports = mongoose.model('Conveniocalusac', ConveniocalusacSchema);
+
+
