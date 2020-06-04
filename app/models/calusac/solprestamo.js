60f3fedf5098704b09d16e92d8a297936c9af0a1
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-06-03 20:18:00 -0600

    Add files via upload

--- app/models/calusac/solprestamo.js
+++ app/models/calusac/solprestamo.js
@@ -21,6 +21,8 @@ var SolprestamoSchema 		=	new  mongoose.Schema({
     requisito8  		: { type : String},
     requisito9  		: { type : String},
     requisito10  		: { type : String},
+    userasignado	: { type : String},
+    userasignadoemail	: { type : String},
     usuarionew	: { type : String },      usuarioup	: { type : String }
    
     }, {
