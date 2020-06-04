1f3cedb0f8ea3047665da0e8b249bf4661167c34
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-06-03 20:37:07 -0600

    Add files via upload

--- app/controllers/calusac/asignacalusac.js
+++ app/controllers/calusac/asignacalusac.js
@@ -1628,14 +1628,7 @@ else{
                                             , function(err, todo) {
                                             if (err){  res.status(404).send(err.message)   }
 
-                                            console.log({ _id:todo._id,
-                                                foto1      	: req.body.foto1        	,
-                                                foto2      	: req.body.foto2        	,
-                                                foto3      	: req.body.foto3        	,
-                                                foto4      	: req.body.foto4        	,
-                                                correo:req.body.correo
-                                              
-                                                })
+                                      
                                             Asignacalusac2.create({ _id:todo._id,
                                             foto1      	: req.body.foto1        	,
                                             foto2      	: req.body.foto2        	,
