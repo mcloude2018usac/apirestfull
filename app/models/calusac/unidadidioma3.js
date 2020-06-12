1cfdb201563a83ba30c0b1f1b0a3d821cb0002b1
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-06-12 15:17:44 -0600

    Add files via upload

--- app/models/calusac/unidadidioma3.js
+++ app/models/calusac/unidadidioma3.js
@@ -1,7 +1,7 @@
 var mongoose =require("mongoose");
 
     var Unidadidioma3Schema 		=	new  mongoose.Schema({
-        idtipounidad   		: { type : String, required : true },
+        idtipounidad   		: { type : String, required : true,ref:'Tipounidad3' },
         idunidadacademica   		: { type : String, required : true },
         codigo	: { type : String, required : true },
         nombre	: { type : String, required : true }
