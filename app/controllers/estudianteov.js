8cc600dd9e5b4f021760e2e59cc233de81672f37
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-06-24 12:43:14 -0600

    Add files via upload

--- app/controllers/estudianteov.js
+++ app/controllers/estudianteov.js
@@ -3,23 +3,7 @@ var Estudianteov = require('../models/estudianteov');
 
 
 exports.getEstudianteov = function(req, res, next){
-    if(req.params.codigo)
-    { 
-        Estudianteov.find({bo_orientacion:req.params.codigo},function(err, todos) {
-            if (err){ res.send(err); }
-        
-            if(todos.length>0)   {    res.json(todos);   }
-            else
-            {  res.status(404).send('NO EXISTE REGISTRO');      }
-            
-        });
-  
-    }
-    else
-    {
-
-    }
-    
+    res.status(404).send('NO EXISTE REGISTRO');   
     
  
 }
