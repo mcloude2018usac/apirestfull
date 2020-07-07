0deafd682b8899508f1fb12cd88b11237d4a59e7
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-07-07 08:00:01 -0600

    Add files via upload

--- app/controllers/calusac/conveniocalusac.js
+++ app/controllers/calusac/conveniocalusac.js
@@ -8,7 +8,7 @@ exports.getConveniocalusac = function(req, res, next){
 
             if(req.params.id3=='todos')
             {
-                Conveniocalusac.find({idempresa:req.params.id},function(err, todos) {
+                Conveniocalusac.find({idempresa:req.params.id,categoria:req.params.id2},function(err, todos) {
                     if (err){ res.send(err); }
                    
                       res.json(todos);   
