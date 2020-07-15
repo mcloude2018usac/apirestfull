6a8dff8d042c0117de5ee7e1a38c16ebf2334574
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-07-15 17:05:48 -0600

    Add files via upload

--- app/models/asociadoventa/asociado.js
+++ app/models/asociadoventa/asociado.js
@@ -8,9 +8,11 @@
   ubicacion		: { type : String, required : true },  
   horario		: { type : String, required : true },  
   nit		: { type : String, required : true },  
+  msmpublicidad: { type : String, required : true },  
   nombrecomercial		: { type : String, required : true },  
   direccioncomercial		: { type : String, required : true },  
   estado		: { type : String, required : true },  
+  foto		: { type : String },  
   usuarionew		: { type : String },  
   usuarioup		: { type : String},  
     }, {
