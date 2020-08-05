329bd517fa6e8154a7d087b5fdec4b92029985a7
mcloude2018usac <44370492+mcloude2018usac@users.noreply.github.com>
2020-08-05 10:25:58 -0600

    Add files via upload

--- app/models/aread_evento.js
+++ app/models/aread_evento.js
@@ -1,19 +1,21 @@
 var mongoose =require("mongoose");
 var Aread_eventoSchema = new mongoose.Schema({
+    idempresa: {        type: String,      required: true     },
+   idtipoevento   		: {     type: String,      required: true  },
+   idarea   		: {     type: String,      required: true  },
 
-   idtipoevento   		: {    id	: { type : String, required : true },   nombre	: { type : String, required : true }  ,   codigo	: { type : String, required : true }      },
-   idarea   		: {    id	: { type : String, required : true },   nombre	: { type : String, required : true }   },
-
-    nombre: {
-        type: String,
-        required: true
-    },
+    nombre: {        type: String,        required: true    },
+    plataforma: {        type: String,        required: true    },
+    url: {        type: String,        required: true    },
+    capacidad: {        type: Number,        required: true    },
     catedratico: {     type: String,      required: true  },
-    dia: {     type: String,      required: true  },
-    horario: {     type: String,      required: true  },
-    edificio: {     type: String,      required: true  },
-    salon: {     type: String,      required: true  }
-    ,usuarionew	: { type : String },      usuarioup	: { type : String }
+    movil: {     type: String,      required: true  },
+    email: {     type: String,      required: true  },
+    fecha: {     type: String,      required: true  },
+
+    clave: {     type: String,      required: true  },
+ 
+    usuarionew	: { type : String },      usuarioup	: { type : String }
    
 }, {
     timestamps: true
