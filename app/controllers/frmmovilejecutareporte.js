6230de1ba9bf6994900cc016de377232db67fd49
Mario Roberto <mario.morales@mcloude.com>
2021-03-03 12:28:49 -0600

    33

--- app/controllers/frmmovilejecutareporte.js
+++ app/controllers/frmmovilejecutareporte.js
@@ -1195,7 +1195,7 @@ function numberWithCommas(x) {
                           ],
                           // optional space between columns
                           columnGap: 1,
-                          margin: [ 45, 83, 10, 1 ] ,
+                          margin: [ 45, 78, 10, 1 ] ,
                           
                          
                           fontSize: 8
