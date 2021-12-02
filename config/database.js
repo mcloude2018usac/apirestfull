56efa4dfdaac884e2e9521c2e071d2d0175f56ea
Mario Roberto <mario.morales@mcloude.com>
2021-12-01 18:39:48 -0600

    aaaa

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //    url:'mongodb://127.0.0.1:27017/ges?retryWrites=true&w=majority'
+  //   url:'mongodb://127.0.0.1:27017/ges?retryWrites=true&w=majority'
  //  url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/mcloude?retryWrites=true&w=majority'
     url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority' // toda lada
  // url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/scep?retryWrites=true&w=majority'
