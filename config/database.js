bdc6bbc08df48a52d85565205e39f00da7eb2e57
Mario Roberto <mario.morales@mcloude.com>
2021-12-12 23:19:06 -0600

    aaa

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     //  url:'mongodb://127.0.0.1:27017/ges?retryWrites=true&w=majority'
- url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/scep?retryWrites=true&w=majority'
- //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/partidocafe?retryWrites=true&w=majority'
+ //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/scep?retryWrites=true&w=majority'
+ url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/partidocafe?retryWrites=true&w=majority'
  //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/hidrotecnica?retryWrites=true&w=majority'
    //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/mcloude?retryWrites=true&w=majority'
  // url:'mongodb+srv://usac:N0T!3n3@cluster0-ylwog.mongodb.net/test?retryWrites=true&w=majority' // toda lada
