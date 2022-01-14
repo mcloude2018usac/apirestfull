bf9e6fa406dc132fef48f0c199b639ad2c36fbe0
Mario Roberto <mario.morales@mcloude.com>
2022-01-13 22:32:26 -0600

    ssss

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     //  url:'mongodb://127.0.0.1:27017/ges?retryWrites=true&w=majority'
- url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/scep?retryWrites=true&w=majority'
-//url:'mongodb+srv://usac:Nu3va3ra2022%40@cluster0.kmuar.mongodb.net/usac2022?retryWrites=true&w=majority' // toda lada
+ //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/scep?retryWrites=true&w=majority'
+url:'mongodb+srv://usac:Nu3va3ra2022%40@cluster0.kmuar.mongodb.net/usac2022?retryWrites=true&w=majority' // toda lada
  //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/partidocafe?retryWrites=true&w=majority'
  //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/hidrotecnica?retryWrites=true&w=majority'
 //url:'mongodb+srv://dbcloude:Admin123@cluster0.emali.mongodb.net/mcloude?retryWrites=true&w=majority'
@@ -12,4 +12,4 @@ module.exports = {
 //url:'mongodb://181.174.122.180:27017/muni?retryWrites=true&w=majority'
 //-----------------------------------------para carnes
 //mongodb+srv://usac:6zdvLMGFvaN7ohHD@cluster0-labwq.mongodb.net/test
-}  
+}    
