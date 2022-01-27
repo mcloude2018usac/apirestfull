96b4ecc7ededac8c12b40f3abfab92ac7152d723
Mario Roberto <mario.morales@mcloude.com>
2022-01-27 07:30:37 -0600

    aaa

--- www/4376.f9faaea70eaf300b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{74376:(h,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(69533),c=s(57639),a=s(67300);let n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,c.b)(this);return(0,o.h)(o.H,{class:(0,a.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
